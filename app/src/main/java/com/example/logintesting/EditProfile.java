7b146f0d5534284f016e1d24143cdb2cac5561b1
Victoria Briceno <97036784+victoriabriceno@users.noreply.github.com>
2022-07-16 14:54:29 -0400

    Gradle

---
+++ app/src/main/java/com/example/logintesting/EditProfile.java
@@ -0,0 +1,14 @@
+package com.example.logintesting;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
