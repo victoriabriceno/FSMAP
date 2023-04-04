a884913b8604ff859414f55d5a89080c022719f1
Victoria Briceno <97036784+victoriabriceno@users.noreply.github.com>
2023-04-04 11:39:06 -0400

    design of favorites and custom

--- app/src/main/java/com/example/FSMap/MapsActivity.java
+++ app/src/main/java/com/example/FSMap/MapsActivity.java
@@ -177,10 +177,6 @@ GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
         setContentView(binding.getRoot());
 
 
-
-
-
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
